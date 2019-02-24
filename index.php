a0aab39990d673e1a4e1a2edb437d5b2befe73d0
mr.lostov <lostov@hi.uz>
2019-02-24 18:45:40 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-define('__botAPI__','672927107:AAEx1phGMwfhdn9ufNOCaLGsMzj5BXwkP58');
-function send($method,$datas=[])
+function RequestSend($method,$datas=[])
 	{
-		$url = "https://api.telegram.org/bot".__botAPI__."/".$method;
+		$url = "https://api.telegram.org/bot672927107:AAEx1phGMwfhdn9ufNOCaLGsMzj5BXwkP58/".$method;
 		$ch = curl_init();
 		curl_setopt($ch,CURLOPT_URL,$url);
 		curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
@@ -17,17 +16,8 @@ function send($method,$datas=[])
 				return json_decode($res);
 			} 
 	}
-$desc = "INFO:
-_______________________
-📞 +998 97 420 9008
-☎️ +998 95 196 9008
-📨  telegram orqali: @internationalStudyService
-_______________________
-📌Manzil: Yunusobod tumani, (Oldingi Toshkent Moliya Instituti binosi hozirgi Toshkent Arxitektura instituti)";
+$desc = "INFO:\n\n_______________________\n📞 +998 97 420 9008\n☎️ +998 95 196 9008\n📨  telegram orqali: @internationalStudyService\n_______________________\n📌Manzil: Yunusobod tumani, (Oldingi Toshkent Moliya Instituti binosi hozirgi Toshkent Arxitektura instituti)";
 $method = 'setChatDescription';
-$datas = [
-  'chat_id' => -1001175758497,
-  'description' => $desc
-];
-var_dum(send($method,$datas));
+$datas = ['chat_id' => -1001175758497,'description' => $desc ];
+var_dum(RequestSend($method,$datas));
 ?>
